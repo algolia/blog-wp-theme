dd2da6a3e3d22053abfe152725312fb1958617de
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-25 23:57:20 -0800

    Add google analytics tracking code

--- footer.php
+++ footer.php
@@ -10,5 +10,20 @@
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
     <?php //wp_footer(); ?>
   </div>
+
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-32446386-1']);
+    _gaq.push(['_setDomainName', 'algolia.com']);
+    _gaq.push(['_trackPageview', '/blog']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 </body>
 </html>
